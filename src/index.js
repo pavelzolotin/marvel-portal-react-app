9b64910c5be9952c71a84f5cf861243f29dfe05d
Pavel Zolotin <zolotinpavel@gmail.com>
2022-07-25 16:50:03 +0500

    add character list display

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+
 import './style/style.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );
