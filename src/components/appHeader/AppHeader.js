b1983d52acd4449495683adebe72c3a31fdcf517
Pavel Zolotin <zolotinpavel@gmail.com>
2022-08-04 16:31:35 +0500

    add react-router v.6 & add changes in components for new version

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -2,7 +2,6 @@ import { Link, NavLink } from 'react-router-dom';
 import './appHeader.scss';
 
 
-
 const AppHeader = () => {
     return (
         <header className="app__header">
@@ -13,9 +12,23 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><NavLink exact activeStyle={{'color': '#9f0013'}} to="/">Characters</NavLink></li>
+                    <li>
+                        <NavLink
+                            end
+                            style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
+                            to="/"
+                        >Characters
+                        </NavLink>
+                    </li>
                     /
-                    <li><NavLink exact activeStyle={{'color': '#9f0013'}} to="/comics">Comics</NavLink></li>
+                    <li>
+                        <NavLink
+                            end
+                            style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
+                            to="/comics"
+                        >Comics
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
